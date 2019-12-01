f332da7c944f649dd1e4b84d49c7d0fb4ecc9262
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2019-12-01 16:19:52 +0200

    Added Categories Screen to Application.

---
+++ meals_app/lib/categories_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/category_item.dart';
+
+import './dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map(
+              (categoryData) => CategoryItem(
+                title: categoryData.title,
+                color: categoryData.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
